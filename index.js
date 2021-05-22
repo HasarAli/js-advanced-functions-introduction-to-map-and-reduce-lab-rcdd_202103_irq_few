44906bb5b5efe45e0f7f62b051fc751e15bf2089
Hasar Ali <hasar.ali@protonmail.com>
2021-05-22 02:40:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 // WHY DOES THIS NOT PASS THE TESTS????!!!!
 // function mapLike(arr, operation = function(x) {return x}) {
 //   arrNew = []
-//   for (let i = 0; i < arr.length; i++) {
-//     arrNew[i] = operation(arr[i])
+//   for (let element of arr) {
+//     arrNew.push(operation(element))
 //   }
 //   return arrNew
 // }
